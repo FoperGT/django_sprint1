35a859d146e9d26b16e5735364cbc3d7b94f2ae0
fopergt <fopergt@gmail.com>
2025-04-10 20:14:17 +1000

    [FIX]

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('pages/', include('pages.urls')),
-    path('', include('blog.urls')),
+    path("admin/", admin.site.urls),
+    path("pages/", include("pages.urls")),
+    path("", include("blog.urls")),
 ]
