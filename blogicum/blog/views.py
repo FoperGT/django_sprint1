8299fa5d4d58b6d2aa024dcf392e0f2c5befa301
fopergt <fopergt@gmail.com>
2025-04-10 20:18:13 +1000

    [FIX]

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -54,4 +54,5 @@ def post_detail(request, pk):
 
 
 def category_posts(request, category_slug):
-    return render(request, "blog/category.html", {"category_slug": category_slug})
+    return render(request, "blog/category.html",
+                  {"category_slug": category_slug})
